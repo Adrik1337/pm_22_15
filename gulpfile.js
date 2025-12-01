62204120db96c01dae9c137eff5cc827d3cb16a9
Adrik1337 <adrik1337.kuzm@gmail.com>
2025-12-01 13:27:09 +0200

    пупупу

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,96 @@
-const { src, dest, watch, parallel } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const cssnano = require ('gulp-cssnano');
+const cssnano = require('gulp-cssnano');
 const browserSync = require('browser-sync').create();
 const imagemin = require('gulp-imagemin');
+const fileInclude = require('gulp-file-include');
+const rename = require('gulp-rename');
 
-// HTML
-function htmlTask() {
-    return src('app/html/**/*.html')
-        .pipe(dest('dist/html'));
+const paths = {
+    html: {
+        src: 'app/*.html',
+        dest: 'dist',
+        },
+    styles: {
+        src: 'app/scss/style.scss',
+        dest: 'dist/css',
+    },
+    scripts: {
+        src: 'app/js/**/*.js',
+        dest: 'dist/js',
+    },
+    images: {
+        src: 'app/img/**/*',
+        dest: 'dist/img',
+    },
+        bootstrap: {
+        css: 'node_modules/bootstrap/dist/css/bootstrap.min.css',
+        js: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+    }
+};
+
+
+const bootstrapCSS = (done) =>  {
+    return src(paths.bootstrap.css)
+        .pipe(dest(paths.styles.dest));
 }
 
-function indexTask() {
-    return src('app/index.html')
-        .pipe(dest('dist'));
+const bootstrapJS = (done) =>  {
+    return src(paths.bootstrap.js)
+        .pipe(dest(paths.scripts.dest));
 }
 
-// SCSS
-function scssTask() {
-    return src('app/scss/**/*.scss')
-        .pipe(sass())
-        .pipe(cssnano())
-        .pipe(dest('dist/css'))
+const htmlTask = (done) =>  {
+    return src(paths.html.src)
+        .pipe(fileInclude({ prefix: '@@', basepath: '@file' }))
+        .pipe(dest(paths.html.dest))
         .pipe(browserSync.stream());
 }
 
-// JS
-function jsTask() {
-    return src('app/js/**/*.js')
-        .pipe(dest('dist/js'));
+const scssTask = (done) =>  {
+    return src(paths.styles.src)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cssnano())
+        .pipe(rename('index.min.css'))
+        .pipe(dest(paths.styles.dest))
+        .pipe(browserSync.stream());
 }
 
-// Images
-function imgTask() {
-    return src('app/img/**/*', { encoding: false })
+const jsTask = (done) =>  {
+    return src(paths.scripts.src)
+        .pipe(dest(paths.scripts.dest))
+        .pipe(browserSync.stream());
+}
+
+const imgTask = (done) =>  {
+    return src(paths.images.src, { encoding: false })
         .pipe(imagemin())
-        .pipe(dest('dist/img'));
-}
-// Reloading
-function serve() {
-    browserSync.init( { server: { baseDir: 'dist' } });
-    watch('app/html/**/*.html', htmlTask).on('change', browserSync.reload);
-    watch('app/scss/**/*.scss', scssTask).on('change', browserSync.reload);
-    watch('app/js/**/*.js', jsTask).on('change', browserSync.reload);
-    watch('app/img/**/*', imgTask).on('change', browserSync.reload);
-    watch('app/index.html', indexTask).on('change', browserSync.reload);
+        .pipe(dest(paths.images.dest))
+        .pipe(browserSync.stream());
 }
 
-exports.default = parallel(htmlTask, indexTask, scssTask, jsTask, imgTask, serve);
+
+const serve = (done) =>  {
+    browserSync.init({
+        server: { baseDir: 'dist' }
+    });
+}
+
+const watcher = (done) => {
+    watch(paths.html.watch, htmlTask);
+    watch(paths.styles.watch, scssTask);
+    watch(paths.scripts.watch, jsTask);
+    watch(paths.images.watch, imgTask);
+}
+
+
+const build = series(
+    bootstrapCSS, 
+    bootstrapJS, 
+    htmlTask, 
+    scssTask, 
+    jsTask, 
+    imgTask
+);
+
+exports.default = series(build, serve, watcher);

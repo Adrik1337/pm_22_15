f5fe4dd9591bbe61ec90e7a0d8d724907ef94d83
Danylo687 <danylodobysh@gmail.com>
2025-12-04 09:30:04 +0200

    Lab 5

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,81 @@
-console.log('working!!!!!!');
+document.addEventListener('DOMContentLoaded', () => {
+
+    /* Data download function */
+    async function loadData() {
+        try {
+            const response = await fetch('data.json');
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
+
+            // Passing the loaded data to the display function
+            initPage(data);
+
+        } catch (error) {
+            console.error('Помилка завантаження data.json:', error);
+        }
+    }
+
+    /* Content generation */
+    function initPage(data) {
+        // Insert name
+        const nameContainer = document.getElementById('personName');
+        if (nameContainer) {
+            const info = data.personalInfo;
+            nameContainer.innerHTML = `<h1 class="cv-name-first">${info.firstName}</h1>
+                                       <h1 class="cv-name-last">${info.lastName}</h1>
+                                       <h1 class="cv-title">${info.role}</h1>`;
+        }
+
+        // Generation of experience
+        renderExperience(data.experience);
+
+        // Click initialization
+        initToggles();
+    }
+
+    function renderExperience(experienceData) {
+        const container = document.getElementById('experience-container');
+        if (!container) return;
+
+        container.innerHTML = '';
+
+        experienceData.forEach(item => {
+            const html = `
+                <div class="mb-4 toggle-content-wrapper"> <h6 class="fw-bold mb-1">${item.title}</h6>
+                    <p class="mb-2">${item.company}/${item.location}/${item.period}</p>
+                    <p class="main-content-wrapper text-muted mb-0">${item.description}</p>
+                </div>
+            `;
+            container.innerHTML += html;
+        });
+    }
+
+    /* Event handling Toggle */
+    function initToggles() {
+        const triggers = document.querySelectorAll('.toggle-trigger');
+
+        triggers.forEach(trigger => {
+            trigger.addEventListener('click', function() {
+                // Find the next element (content)
+                const content = this.nextElementSibling;
+                // Find the arrow inside the title
+                const arrow = this.querySelector('.arrow-icon');
+
+                // Switch classes
+                if (content) {
+                    content.classList.toggle('hidden');
+                }
+                if (arrow) {
+                    arrow.classList.toggle('rotate');
+                }
+            });
+        });
+    }
+
+    // Start downloading
+    loadData();
+});
--- gulpfile.js
+++ gulpfile.js
@@ -10,26 +10,31 @@ const paths = {
     html: {
         src: 'app/*.html',
         dest: 'dist',
-        watch: 'app/**/*.html' // Додано
+        watch: 'app/**/*.html'
     },
     styles: {
         src: 'app/scss/style.scss',
         dest: 'dist/css',
         watch: 'app/scss/**/*.scss'
     },
-    scripts: {
-        src: 'app/js/**/*.js',
-        dest: 'dist/js',
-        watch: 'app/js/**/*.js'
-    },
     images: {
         src: 'app/img/**/*',
         dest: 'dist/img',
         watch: 'app/img/**/*'
     },
+    scripts: {
+        src: 'app/js/**/*.js',
+        dest: 'dist/js',
+        watch: 'app/js/**/*.js'
+    },
     bootstrap: {
         css: 'node_modules/bootstrap/dist/css/bootstrap.min.css',
         js: 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+    },
+    json: {
+        src: 'app/data.json',
+        dest: 'dist',
+        watch: 'app/data.json'
     }
 };
 
@@ -58,16 +63,23 @@ const scssTask = () => {
         .pipe(dest(paths.styles.dest))
         .pipe(browserSync.stream());
 }
+
+const imgTask = () =>  {
+    return src(paths.images.src, { encoding: false })
+        .pipe(imagemin())
+        .pipe(dest(paths.images.dest))
+        .pipe(browserSync.stream());
+}
+
 const jsTask = () =>  {
     return src(paths.scripts.src)
         .pipe(dest(paths.scripts.dest))
         .pipe(browserSync.stream());
 }
 
-const imgTask = () =>  {
-    return src(paths.images.src, { encoding: false })
-        .pipe(imagemin())
-        .pipe(dest(paths.images.dest))
+const jsonTask = () => {
+    return src(paths.json.src)
+        .pipe(dest(paths.json.dest))
         .pipe(browserSync.stream());
 }
 
@@ -86,10 +98,11 @@ const serve = (done) =>  {
 const watcher = () => {
     watch(paths.html.watch, series(htmlTask, reload));
     watch(paths.styles.watch, series(scssTask, reload));
-    watch(paths.scripts.watch, series(jsTask, reload));
     watch(paths.images.watch, series(imgTask, reload));
+    watch(paths.scripts.watch, series(jsTask, reload));
+    watch(paths.json.watch, series(jsonTask, reload));
 }
 
-const build = series( bootstrapCSS, bootstrapJS, htmlTask, scssTask, jsTask, imgTask );
+const build = series( bootstrapCSS, bootstrapJS, htmlTask, scssTask, jsTask, imgTask, jsonTask );
 
 exports.default = series( build, serve, watcher );
